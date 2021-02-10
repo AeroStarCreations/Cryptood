e7721201b402b50941425dfa2eca81dd2ee4eb4c
Nathan Balli <AeroStarCreations1@gmail.com>
2021-02-09 21:59:53 -0800

    Use feedparser to parse CoinDesk RSS feed

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # It all starts here
+
+import feedparser
+
+coin_desk_feed = feedparser.parse('https://www.coindesk.com/feed')
+
+first_entry = coin_desk_feed.entries[0]
+
+for key, value in first_entry.items():
+    print(f'{key}:\t{value}')
