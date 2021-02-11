769139a925b48107510cc408661a46bcc071894b
Nathan Balli <AeroStarCreations1@gmail.com>
2021-02-10 21:11:27 -0800

    Added sentiment and word count output

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 # It all starts here
 
 import feedparser
+import time
+import re
+from textblob import TextBlob
+
+start_time = time.time()
+buy_count = 0
+sell_count = 0
 
 coin_desk_feed = feedparser.parse('https://www.coindesk.com/feed')
 
-first_entry = coin_desk_feed.entries[0]
+for entry in coin_desk_feed.entries:
+    text = entry.title + entry.summary
+    blob = TextBlob(text)
+    buy_count += len(re.findall(r'buy\w*', text, flags=re.I))
+    sell_count += len(re.findall(r'sell\w*', text, flags=re.I))
+    print(f'Title: {entry.title}')
+    print(f'Summary: {entry.summary}')
+    print(f'Sentiment: {blob.sentiment}\n')
 
-for key, value in first_entry.items():
-    print(f'{key}:\t{value}')
+print(f'\nBuy count: {buy_count}')
+print(f'Sell count: {sell_count}')
+print(f'\nRuntime: {time.time()-start_time} seconds')
+
+# first_entry = coin_desk_feed.entries[0]
+
+# print(f'len: {len(coin_desk_feed.entries)}')
+
+# for key, value in first_entry.items():
+#     print(f'{key}:\t{value}')
